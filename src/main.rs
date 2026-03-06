43c49bf6dc737c0294a120c3fb374d4a856db9f8
Dragonink <Dragonink@users.noreply.github.com>
2026-03-06 18:55:21 +0100

    feat: Set up rootcause

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 #![doc = env!("CARGO_PKG_DESCRIPTION")]
 
-fn main() {
-	println!("Hello, world!");
+use std::process::ExitCode;
+
+/// Main function
+///
+/// # Errors
+/// Returns an error if:
+fn run() -> rootcause::Result<()> {
+	Ok(())
+}
+
+#[expect(clippy::print_stderr, reason = "display error report")]
+fn main() -> ExitCode {
+	match run() {
+		Ok(()) => ExitCode::SUCCESS,
+		Err(err) => {
+			eprintln!("{err}");
+			ExitCode::FAILURE
+		}
+	}
 }
