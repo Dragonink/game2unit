16df8d46db9d01dbd23cd8707de991a351bd9b38
Dragonink <Dragonink@users.noreply.github.com>
2026-03-20 13:49:33 +0100

    feat: Setup logger

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 #![doc = env!("CARGO_PKG_DESCRIPTION")]
 
 fn main() {
-	println!("Hello, world!");
+	setup_logger();
+
+	log::info!("Hello, world!");
+}
+
+/// Configures and installs the application logger.
+///
+/// # Panics
+/// Panics if a logger has previously been installed.
+fn setup_logger() {
+	use log::LevelFilter;
+
+	env_logger::Builder::new()
+		.format_indent(Some(8))
+		.format_target(false)
+		.filter_level(if cfg!(debug_assertions) {
+			LevelFilter::max()
+		} else {
+			LevelFilter::Warn
+		})
+		.parse_env("GAME2UNIT_LOG")
+		.init();
 }
