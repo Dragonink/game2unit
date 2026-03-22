1765c62ef8714ab11decc0db472cab1fcbd50e8a
Dragonink <Dragonink@users.noreply.github.com>
2026-03-22 12:45:06 +0100

    feat: Execute command passed as program args

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Game2Unit
 Launch games as systemd user units.
 
+## Usage
+Just prepend `game2unit` to the usual command that launches your game:
+```
+game2unit <program> [<program args>...]
+```
+
+> [!IMPORTANT]
+> If the command involves settings environment variables like this:
+> ```sh
+> FOO=foo BAR=bar game --game-option
+> ```
+>
+> You will need to put the variables before `game2unit`:
+> ```sh
+> FOO=foo BAR=bar game2unit game --game-option
+> ```
+> or use the [`env`](https://www.man7.org/linux/man-pages/man1/env.1.html) command:
+> ```sh
+> game2unit env FOO=foo BAR=bar game --game-option
+> ```
+
 ## Acknowledgements
 - This project originally started as a wrapper around [App2Unit](https://github.com/Vladimir-csp/app2unit) (❤️) before being rewritten to talk directly to systemd via D-Bus.
   That's the reason why the name "Game2Unit" is (heavily) inspired by "App2Unit".
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,59 @@
 #![doc = env!("CARGO_PKG_DESCRIPTION")]
 
-use rootcause as _;
 
-fn main() {
+use std::{ffi::OsStr, process::ExitCode};
+
+use rootcause::{option_ext::OptionExt as _, prelude::*};
+
+fn main() -> ExitCode {
+	/// Part of the [`main()`] function that makes the program fails if an error occurs.
+	///
+	/// On success this function will not return,
+	/// see [`CommandExt::exec()`](std::os::unix::process::CommandExt::exec()) for more details.
+	///
+	/// # Errors
+	/// Returns an error if:
+	/// - executing the command fails.
+	fn critical_main() -> rootcause::Result<()> {
+		let mut args = std::env::args_os().skip(1);
+
+		exec(args).context("Failed to execute command")?;
+		unreachable!();
+	}
+
 	setup_logger();
 
-	log::info!("Hello, world!");
+	match critical_main() {
+		Ok(()) => unreachable!(),
+		Err(err) => {
+			log::error!("Fatal error: {err}");
+			ExitCode::FAILURE
+		}
+	}
+}
+
+/// Executes the given command, **replacing this process**.
+///
+/// On success this function will not return,
+/// see [`CommandExt::exec()`](std::os::unix::process::CommandExt::exec()) for more details.
+///
+/// # Errors
+/// Returns an error if:
+/// - `args` is empty;
+/// - the execution of the constructed command fails.
+fn exec<A, I>(args: I) -> rootcause::Result<()>
+where
+	A: AsRef<OsStr>,
+	I: IntoIterator<Item = A>,
+{
+	use std::{os::unix::process::CommandExt as _, process::Command};
+
+	let mut args = args.into_iter();
+	let mut cmd = Command::new(args.next().context("Missing game executable")?);
+	cmd.args(args);
+
+	log::info!("Executing command:\n{cmd:?}");
+	Err(report!(cmd.exec()).into_dynamic())
 }
 
 /// Configures and installs the application logger.
